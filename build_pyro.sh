8a6eead6c9067291f22a089c8ba78d3cef5ee117
Vladimir A. Nosenko <vn@cypodgroup.com>
2018-01-26 12:35:54 +0100

    simplify choose between Fedora/Ubuntu

--- build_pyro.sh
+++ build_pyro.sh
@@ -1,2 +1,3 @@
-docker build --build-arg GITREPO="git://git.yoctoproject.org/poky" --build-arg BRANCH="pyro" -t toaster .
+docker build --build-arg GITREPO="git://git.yoctoproject.org/poky" --build-arg BRANCH="pyro" -t crops/toaster:fedora-27 . -f$1
+
 
