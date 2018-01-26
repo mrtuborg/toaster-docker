a47c5163b3793cf4792e88b12bcf2420e77db93e
Vladimir A. Nosenko <vn@cypodgroup.com>
2018-01-26 21:05:22 +0100

    build_pyro.sh: create image tag from Dockerfile name

--- build_pyro.sh
+++ build_pyro.sh
@@ -1,3 +1,3 @@
-docker build --build-arg GITREPO="git://git.yoctoproject.org/poky" --build-arg BRANCH="pyro" -t crops/toaster:fedora-27 . -f$1
+docker build --build-arg GITREPO="git://git.yoctoproject.org/poky" --build-arg BRANCH="pyro" -t crops/toaster:$1 . -f$1
 
 
